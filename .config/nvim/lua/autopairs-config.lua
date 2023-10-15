26aaceb52445299226cf345575a3c499761181bc
gryzus <gryzus24@gmail.com>
2023-10-15 02:04:28 +0200

    update neovim config

--- .config/nvim/lua/autopairs-config.lua
+++
@@ -1,9 +0,0 @@
-local npairs = require('nvim-autopairs')
-local Rule = require('nvim-autopairs.rule')
-
-npairs.setup({})
-npairs.clear_rules()
-npairs.add_rules {
-    Rule('{', '}')
-        :end_wise(function() return true end)
-}
--- .config/nvim/lua/treesitter-config.lua
+++ .config/nvim/lua/treesitter-config.lua
@@ -1,23 +1,20 @@
 require('nvim-treesitter.configs').setup({
-    ensure_installed = {"c", "python", "zig"},
+    ensure_installed = {'c', 'python', 'zig'},
     -- ignore_install = {},
     highlight = {
     	enable = true,
     },
     indent = {
         enable = true,
-        disable = {"python"},
+        disable = {'python'},
     },
     incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<C-n>",
-          node_incremental = "<C-n>",
-          scope_incremental = "<C-s>",
-          node_decremental = "<C-p>",
+          init_selection = '<C-n>',
+          node_incremental = '<C-n>',
+          scope_incremental = '<C-s>',
+          node_decremental = '<C-p>',
         },
-    },
-    autopairs = {
-    	enable = true,
     },
 })
