8913e19920b0db438df056c1e36d033beb1974fc
gryzus <gryzus24@gmail.com>
2024-03-08 20:43:18 +0100

    add -DPATH_PREFIXES compile time knob to the cgb program
    -DPATH_PREFIXES makes it possible to specify (as command line
     arguments) directory paths whose dirnames represent the
     .git/HEAD search termination point, might save a few getcwd
     syscalls, e.g.
      $ cgb ~ /mnt
     cgb will only check for repositories in directories under
     /home/user or /mnt (and /home/user and /mnt itself)
    
    * cleanup and optimize remaining suboptimal type conversions
    * use fixed types matching the x86_64 ABI
    * use -march=native for compilation
    * change the base address by one page down in the linker script
      I will not hazard a speculation as to why it makes the program
      finish executing in less cycles...

--- bin/src/cgb/build.sh
+++ bin/src/cgb/build.sh
@@ -1,9 +1,17 @@
 #!/bin/dash
 
+BUILD_FLAGS=
+if [ "$1" = "-DPATH_PREFIXES" ]; then
+    BUILD_FLAGS="$1"
+fi
+
 (set -x;
-    cc -Wall -Wextra -Wpedantic -Wconversion -Wdeclaration-after-statement \
+    cc -std=c11 \
+    -Wall -Wextra -Wpedantic -Wconversion -Wdeclaration-after-statement \
     -s -static -nostdlib \
-    -fno-stack-protector -O3 \
+    -march=native \
+    -O3 -fno-stack-protector \
+    $BUILD_FLAGS \
     -T cgb.ld \
     cgb.c -o cgb
 )
--- bin/src/cgb/cgb.c
+++ bin/src/cgb/cgb.c
@@ -4,33 +4,28 @@
 
 #include <asm-generic/fcntl.h> /* O_{RDONLY,NOATIME} */
 #include <asm/unistd_64.h>
+#ifdef PATH_PREFIXES
+# include <limits.h>
+#endif
 #include <stdbool.h>
+#include <stdint.h>
 
 #define arr_count(a) (sizeof(a) / sizeof((a)[0]))
-#define unlikely(x) __builtin_expect(!!(x), 0)
 #define noreturn __attribute__((noreturn))
+#define unlikely(x) __builtin_expect(!!(x), 0)
 
-static noreturn void exit(int code)
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef int64_t i64;
+
+static i64 syscall_check(u64 r)
 {
-    __asm__ volatile (
-        "syscall"
-        :
-        : "a"(__NR_exit), "D"(code)
-        :
-    );
-    __builtin_unreachable();
+    return r > -4096UL ? -1 : (i64)r;
 }
 
-static long syscall_check(unsigned long r)
+static u64 syscall3(i64 nr, i64 a, i64 b, i64 c)
 {
-    if (r > -4096UL)
-        return -1;
-    return (long)r;
-}
-
-static unsigned long syscall3(long nr, long a, long b, long c)
-{
-    unsigned long ret;
+    u64 ret;
 
     __asm__ volatile (
         "syscall"
@@ -41,19 +36,19 @@ static unsigned long syscall3(long nr, long a, long b, long c)
     return ret;
 }
 
-static long read(int fd, char *buf, unsigned long n)
+static i64 read(u32 fd, char *buf, u64 n)
 {
-    return syscall_check(syscall3(__NR_read, fd, (long)buf, (long)n));
+    return syscall_check(syscall3(__NR_read, fd, (i64)buf, (i64)n));
 }
 
-static long write(int fd, const char *buf, unsigned long n)
+static i64 write(u32 fd, const char *buf, u64 n)
 {
-    return syscall_check(syscall3(__NR_write, fd, (long)buf, (long)n));
+    return syscall_check(syscall3(__NR_write, fd, (i64)buf, (i64)n));
 }
 
-static long open_rdonly(const char *path)
+static i64 open_rdonly(const char *path)
 {
-    unsigned long ret;
+    u64 ret;
 
     __asm__ volatile (
         "syscall"
@@ -64,9 +59,9 @@ static long open_rdonly(const char *path)
     return syscall_check(ret);
 }
 
-static long getcwd(char *buf, unsigned long size)
+static i64 getcwd(char *buf, u64 size)
 {
-    unsigned long ret;
+    u64 ret;
 
     __asm__ volatile (
         "syscall"
@@ -80,21 +75,24 @@ static long getcwd(char *buf, unsigned long size)
 #define GITHEAD ".git/HEAD"
 #define GITHEAD_SIZE 10
 
-#define BRANCH_FOUND 0
-#define BRANCH_NOT_FOUND 1
-#define GETCWD_ROOT_OR_FAILED 2
-#define GITHEAD_READ_FAILED 3
+#define BRANCH_WRITTEN        0
+#define PATH_AT_ROOT          1
+#define PATH_BEFORE_PREFIX    2
+#define PATH_UNWANTED_PREFIX  3
+#define GITHEAD_BAD           253
+#define GETCWD_BAD            254
+#define ARGC_BAD              255
 
-static void write_branch_name(char *buf256, int githead_fd)
+static int write_branch_name(char *buf256, u32 githead_fd)
 {
-    char *ptr;
     char *const start255 = buf256 + 1;
+    char *ptr;
 
     /* read into buf:  [0 A A A A A \n 0 0 ...]
      * overwrite with: [( A A A A A  )   0 ...] */
-    const long nread = read(githead_fd, start255, 255 - 1);
+    const i64 nread = read(githead_fd, start255, 255 - 1);
     if (unlikely(nread < 2 || start255[nread - 1] != '\n'))
-        exit(GITHEAD_READ_FAILED);
+        return GITHEAD_BAD;
 
     ptr = start255 + nread;
 
@@ -104,58 +102,158 @@ static void write_branch_name(char *buf256, int githead_fd)
         --ptr;
     *ptr = '(';
 
-#if 1
-    write(1, ptr, (unsigned long)(start255 + nread - ptr + 1));
-#else
-    /* GCC seems to generate an unnecessary movzx in the while loop
-     * after parenthesizing this calculation - possible bug? */
-    write(1, ptr, (start255 + nread) - (ptr - 1));
-#endif
+    write(1, ptr, (u64)(start255 + nread) - (u64)(ptr - 1));
+
+    return BRANCH_WRITTEN;
 }
 
-static bool is_dir_home(const char *s, long i)
+#ifdef PATH_PREFIXES
+static u64 strlen(const char *s)
 {
-    if (i == 6) {
-        return (
-               s[i - 2] == 'e'
-            && s[i - 3] == 'm'
-            && s[i - 4] == 'o'
-            && s[i - 5] == 'h'
-        );
+    const char *p = s;
+    for (; *p; ++p)
+        ;
+    return (u64)(p - s);
+}
+
+static bool hasprefix(const char *s, const char *p)
+{
+    for (; *p; ++s, ++p)
+        if (*s != *p)
+            return false;
+
+    return true;
+}
+
+int _main(i64 *const rsp)
+{
+    char buf[256];
+    i64 ret;
+    u64 diri;
+
+    const char **argv;
+    u64 argc;
+    u64 prefixlen_min = UINT64_MAX;
+
+    if (unlikely(*rsp <= 0))
+        return ARGC_BAD;
+
+    /* skip first argument */
+    argc = (u64)(*rsp - 1);
+    argv = (void *)(rsp + 2);
+
+    /* diri stores the index of a path separator + 1 */
+    ret = getcwd(buf, arr_count(buf) - GITHEAD_SIZE - 1);
+    if (unlikely(ret < 3 || buf[0] != '/'))
+        return GETCWD_BAD;
+    diri = (u64)ret;
+
+    /* replace '\0' with '/' */
+    buf[diri - 1] = '/';
+    buf[diri] = '\0';
+
+    if (argc > 0) {
+        bool prefix_ok = false;
+        for (u64 i = 0; i < argc; ++i) {
+            if (hasprefix(buf, argv[i])) {
+                u64 len = strlen(argv[i]);
+                if (len < prefixlen_min)
+                    prefixlen_min = len;
+                prefix_ok = true;
+            }
+        }
+        if (!prefix_ok)
+            return PATH_UNWANTED_PREFIX;
     }
-    return false;
+
+    do {
+        i64 fd;
+
+        for (u64 i = 0; i < GITHEAD_SIZE; ++i)
+            buf[diri + i] = GITHEAD[i];
+
+        fd = open_rdonly(buf);
+        if (fd != -1)
+            return write_branch_name(buf, (u32)fd);
+
+        /* lookahead is safe, we know that buf[0] == '/' */
+        while (buf[--diri - 1] != '/')
+            ;
+
+        if (prefixlen_min != UINT64_MAX && prefixlen_min > diri)
+            return PATH_BEFORE_PREFIX;
+
+    } while (diri > 1);
+
+    /* reached /, assume there is no /.git directory and exit */
+    return PATH_AT_ROOT;
 }
 
 noreturn void _start(void)
 {
-    char buf[256];
-    long i, j, fd;
+    /* obey the SysV ABI reluctantly */
+    __asm__ (
+        "xor %ebp,%ebp\n"  /* zero the stack frame */
+        "mov %rsp,%rdi\n"  /* (%rsp) holds the argc */
+        "and $-16,%rsp\n"  /* %rsp must be 16-byte aligned */
+        "call _main\n"
+        "mov %eax,%edi\n"
+        "mov $60,%eax\n"   /* sys_exit, exit code in %edi */
+        "syscall\n"
+    );
+    __builtin_unreachable();
+}
+#else
+static bool is_dir_home(const char *s, u64 diri)
+{
+    if (diri == 6) {
+        u32 t;
+        __builtin_memcpy(&t, s + 1, 4);
+        return t == 0x656d6f68;
+    }
+    return false;
+}
 
-    /* i stores the index of a path separator + 1 */
-    i = getcwd(buf, arr_count(buf) - GITHEAD_SIZE);
-    if (unlikely(i < 3 || buf[0] != '/'))
-        exit(GETCWD_ROOT_OR_FAILED);
+static int _main(void)
+{
+    char buf[256];
+    i64 ret;
+    u64 diri;
+
+    /* diri stores the index of a path separator + 1 */
+    ret = getcwd(buf, arr_count(buf) - GITHEAD_SIZE - 1);
+    if (unlikely(ret < 3 || buf[0] != '/'))
+        return GETCWD_BAD;
+    diri = (u64)ret;
 
     /* replace '\0' with '/' */
-    buf[i - 1] = '/';
+    buf[diri - 1] = '/';
+    buf[diri] = '\0';
 
     do {
-        for (j = 0; j < GITHEAD_SIZE; ++j)
-            buf[i + j] = GITHEAD[j];
+        i64 fd;
+
+        for (u64 i = 0; i < GITHEAD_SIZE; ++i)
+            buf[diri + i] = GITHEAD[i];
 
         fd = open_rdonly(buf);
-        if (fd != -1) {
-            write_branch_name(buf, (int)fd);
-            exit(BRANCH_FOUND);
-        }
+        if (fd != -1)
+            return write_branch_name(buf, (u32)fd);
 
-        /* we know that buf[0] == '/' */
-        while (buf[--i - 1] != '/')
+        /* lookahead is safe, we know that buf[0] == '/' */
+        while (buf[--diri - 1] != '/')
             ;
 
-    } while (i > 1 && !is_dir_home(buf, i));
+    } while (diri > 1 && !is_dir_home(buf, diri));
 
     /* reached / or /home,
      * assume there is no /.git nor /home/.git directory and exit */
-    exit(BRANCH_NOT_FOUND);
+    return PATH_AT_ROOT;
 }
+
+noreturn void _start(void)
+{
+    __asm__ volatile ("syscall" : /* */ : "a"(__NR_exit), "D"(_main()));
+    __builtin_unreachable();
+}
+#endif /* PATH_PREFIXES */
