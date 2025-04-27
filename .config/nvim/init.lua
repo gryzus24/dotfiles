ab9193e4a070a48fbb2d3588271a012c95843433
gryzus <gryzus24@gmail.com>
2025-04-27 12:43:38 +0200

    add neovide config
    * add copy/paste mappings.
    * disable more ugly animations, the only pleasant and useful animation
      in this gui is the smooth scrolling.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -256,6 +256,10 @@ if vim.g.neovide then
     vim.g.neovide_cursor_animation_length = 0
     vim.g.neovide_cursor_antialiasing = 0
     vim.g.neovide_cursor_trail_size = 0
+    vim.g.neovide_position_animation_length = 0
     vim.g.neovide_scroll_animation_length = 0.1
-    vim.o.guifont = 'Go Mono:b:h16:w-1'
+
+    vim.keymap.set('n', '<cs-v>', '"+P')
+    vim.keymap.set('i', '<cs-v>', '<space><esc>v"+Pa')
+    vim.keymap.set('v', '<cs-c>', '"+ygv')
 end
