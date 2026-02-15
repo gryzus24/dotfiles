315c905bb7c868cbcf242b65734c51af19f06e95
gryzus <gryzus24@gmail.com>
2026-02-15 16:33:56 +0100

    update neovim config
    * update the telescope plugin to the latest version.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -182,8 +182,7 @@ require('lazy').setup({
         },
         {
             'nvim-telescope/telescope.nvim',
-            tag = '0.1.8',
-            branch = '0.1.x',
+            version = '*',
             dependencies = {'nvim-lua/plenary.nvim'},
         },
         {'nvim-treesitter/nvim-treesitter'},
